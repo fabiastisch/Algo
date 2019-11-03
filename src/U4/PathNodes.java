4721912b6188f6aea789c801d1f170b6636de6a1
Fabian Weber <20weber.fabian00@googlemail.com>
2019-11-03 19:17:53 +0100

    fertig

--- src/U4/PathNodes.java
+++ src/U4/PathNodes.java
@@ -1,10 +1,12 @@
 package U4;
 
 import java.math.BigInteger;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * @author Fabian Weber , Robin Schlund, Julian Beitinger
- *         202224       , 202961       , 202387
+ * 202224       , 202961       , 202387
  */
 
 public class PathNodes {
@@ -15,7 +17,7 @@ public class PathNodes {
      * Hinweis: Die Anzahl dieser Punkte fur ¨ n = 4 ist 28, fur ¨ n = 5 ist sie 152.
      */
     static int n, i;
-    static HashMap<String,BigInteger[]> memory = new HashMap<>();
+    static HashMap<String, BigInteger[]> memory = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -28,48 +30,44 @@ public class PathNodes {
                 System.err.println("NumberFormatExeption");
             }
 
-            //   for (int i = 0; i <= n; i++) {
 
-                BigInteger[] solution = nodes(n, 0, 0);
-                System.out.println("Für n = " + i + "\n Die Anzahl der verschiedenen Pfade betragen: \n\t " + solution[0]);
-                System.out.println("Die Anzahl der summierten Punkte über sämtliche Pfade beträgt: \n\t " + solution[1]);
+            BigInteger[] solution = nodes(n, 0, 0);
+            System.out.println("Für n = " + n + "\n Die Anzahl der verschiedenen Pfade betragen: \n\t " + solution[0]);
+            System.out.println("\nDie Anzahl der summierten Punkte über sämtliche Pfade beträgt: \n\t " + solution[1]);
 
-            //   }
-            System.out.println("\n\n\n END OF FILE");
-
-        }while (n!=-1);
+        } while (n != -1);
     }
 
-    static BigInteger[] nodes(int x, int y, int t){
+    static BigInteger[] nodes(int x, int y, int t) {
 
-        if (x < 0 || y < 0 || y > x) { // Sollte nie true sein,  TODO: kann evtl. gelöscht werden
+       /* if (x < 0 || y < 0 || y > x) { // Sollte nie true sein,  TODO: kann evtl. gelöscht werden
             System.err.println("Fehler \t"+ + ++i  + "\t" + ""+x+"|"+y+"|"+t);
 
             return new BigInteger[]{BigInteger.ZERO, BigInteger.ZERO};
-        }
+        }*/
         if (x == 0 && y == 0) {
             return new BigInteger[]{BigInteger.ONE, BigInteger.ONE};
         }
-        String s = ""+x+"|"+y+"|"+t;
-        if (memory.containsKey(s))return memory.get(s);
+        String s = "" + x + "|" + y + "|" + t;
+        if (memory.containsKey(s)) return memory.get(s);
 
         BigInteger paths = BigInteger.ZERO;
         BigInteger nodes = BigInteger.ZERO;
 
         if (y - x < 0) {
             BigInteger[] left = nodes(x - 1, y, 1);
-           paths = paths.add(left[0]);  // nach links
-           nodes = nodes.add(left[1]);
+            paths = paths.add(left[0]);  // nach links
+            nodes = nodes.add(left[1]);
         }
-        if (t != 1 && x-y > 1){
+        if (t != 1 && x - y > 1) {
 
-            BigInteger[] leftTop =nodes(x-1,y+1,2);
+            BigInteger[] leftTop = nodes(x - 1, y + 1, 2);
             paths = paths.add(leftTop[0]); // Nach links oben
             nodes = nodes.add(leftTop[1]);
         }
         if ((t == 1 || t == 3) && y != 0) {
 
-            BigInteger[] rightBot = nodes(x+1,y-1,3);
+            BigInteger[] rightBot = nodes(x + 1, y - 1, 3);
             paths = paths.add(rightBot[0]); // Nach rechts unten
             nodes = nodes.add(rightBot[1]);
         }
